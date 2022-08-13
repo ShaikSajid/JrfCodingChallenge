11a5551e74e017ed4a17e3e97f9fbfdee1c3e5f3
ShaikSajid <sajid.shaik147@gmail.com>
2022-08-13 14:33:15 +0530

    small changes for validations

--- controller/loanController.js
+++ controller/loanController.js
@@ -1,10 +1,14 @@
-import inputshema from "../validation/input.shema.js";
+import {shcehma} from "../validation/input.shema.js";
 import {cities,creditScoreCriteria,getSchedule,loanTenure,getAge,convertDate} from "../util/loanUtils.js"
 
 
 export const getLoan =  (req, res, next) => {
     try {
-        var {error} =  inputshema.validateInput(req.body);
+        let {value, error} = shcehma.validate(req.body);
+        if(error){
+            res.status(400).json({ message: error.message });
+        }
+        else{
         const {name, dob, city, credit_score, loan_amount} = req.body;
         const age = getAge(dob);
         const findCity = cities.find(({name}) => name === req.body.city);
@@ -17,9 +21,9 @@ export const getLoan =  (req, res, next) => {
             res.status(200).send(getSchedule(creditScore , loan_amount));
         }
 
-
+    }
     } catch (error) {
-        res.status(400).json({ message: error.message })
+        res.status(400).json({ message: error.message });
     }
 
 
--- validation/input.shema.js
+++ validation/input.shema.js
@@ -1,13 +1,8 @@
 import joi from 'joi';
-const shcehma =  joi.object({
+export const shcehma =  joi.object({
         name: joi.string().required(),
         dob: joi.date().max(Date.now()).message("Date of birth should be less than todays date").required(),
         city: joi.string().required(),
         credit_score: joi.number().integer().max(900).message("Credit score shoul be less than 900").required(),
         loan_amount: joi.number().integer().min(50000).message("Loan amount should be greater than 50000").max(500000).message("Loan amount should be less than 500000").required()
-    })
-
-    async function validateInput(object){
-            return await shcehma.validateAsync(object);
-    }
-export default {validateInput} 
+    });
